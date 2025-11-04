6100533b3db88454b81936e379ec8d1b83473e7e
Choklitos21 <diego.mm09@hotmail.com>
2025-11-04 07:46:41 -0500

    Create repository, add inventory.py and README.md

---
+++ inventory.py
@@ -0,0 +1,34 @@
+#We create our variables
+name = ""
+price = 0.0
+amount = 0
+totalCost = 0.0
+
+#This function checks if the input is a string containing only letters
+def is_Str(text):
+    result = text
+    if not result.isalpha():
+            result = is_Str(input("Invalid input. Please enter a valid name (letters only): "))
+    return result
+
+#This function checks if the input is a non-negative number
+def is_valid(number):
+    result = number
+    if result < 0:
+            result = is_valid(float(input("Invalid input. Please enter a non-negative value: ")))
+    return result
+
+#Here we set our variables using the functions above
+name = is_Str(str(input("Enter the product name: ")))
+price = is_valid(float(input("Enter the product price: ")))
+amount = is_valid(int(input("Enter the product amount: ")))
+
+#This function calculates the total cost that is price multiplied by amount
+def total_cost(price, amount):
+    return price * amount   
+
+#Here we call the function to calculate total cost
+totalCost = total_cost(price, amount)
+
+#Finally, we print the results
+print(f"Product name: {name}, Price: {price}, Amount: {amount}, Total Cost: {totalCost}")
